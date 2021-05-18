a768caa9ee3ced66e05b1cf22465ccd917082ea5
wangfeng0301 <fengwang0301@163.com>
2021-05-18 21:58:28 +0800

    排序算法，未完成

--- DataStructure/inc/sort.h
+++ DataStructure/inc/sort.h
@@ -1,28 +1,57 @@
 #ifndef __SORT_H__
 #define __SORT_H__
 #include "type.h"
+#include <iostream>
+#include "heap.h"
+using namespace std;
 
-/*记录(节点)结构体*/
-typedef struct
+template <typename T>
+class Record
 {
-	int SortKey;
-	int next;
-}Record;
-
-/*堆结构体*/
-typedef struct 
+public:
+	Record();
+	~Record();
+	int key;										//键值，用来做排序比较
+	T dat;											//数据
+};
+template <typename T>
+class Sort:public Record<T>
 {
-	Record *heapArray;								//存放堆数据的数组
-	int CurrentSize;								//当前堆中元素数目
-	int MaxSize;									//堆的最大元素数目
-}SortHeap;
-
-/*静态队列结构体*/
-typedef struct  
-{
-	int head;
-	int tail;
-}StaticQueue;
+public:
+	Sort();
+	~Sort();
+	void insertSort(Record<T> *arr, int n);
+	void modInsertSort(Record<T> *arr,int n,int delta);
+	void shellSort(Record<T> *arr,int n);
+	void selectSort(Record<T> *arr,int n);
+	void heapSort(Record<T> *arr,int n);
+	void bubbleSort(Record<T> *arr,int n);
+	void quickSort(Record<T> *arr,int left,int right);
+	void mergeSort(Record<T> *arr,Record<T> *temparr,int left,int right);
+	void modMergeSort(Record<T> *arr,Record<T> *temparr,int left,int right);
+	void BucketSort(Record<T> *arr,int n,int max);
+};
+///*记录(节点)结构体*/
+//typedef struct
+//{
+//	int SortKey;
+//	int next;
+//}Record;
+//
+///*堆结构体*/
+//typedef struct 
+//{
+//	Record *heapArray;								//存放堆数据的数组
+//	int CurrentSize;								//当前堆中元素数目
+//	int MaxSize;									//堆的最大元素数目
+//}SortHeap;
+//
+///*静态队列结构体*/
+//typedef struct  
+//{
+//	int head;
+//	int tail;
+//}StaticQueue;
 
 void testSort(void);
 #endif
